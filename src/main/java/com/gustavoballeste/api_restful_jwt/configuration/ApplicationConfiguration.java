73e3e7092a799366a9788af8544295b888a4ca07
Gustavo Ballesté Prado <gustavoballeste@gmail.com>
2018-08-21 00:12:41 -0300

    Message properties

---
+++ src/main/java/com/gustavoballeste/api_restful_jwt/configuration/ApplicationConfiguration.java
@@ -0,0 +1,9 @@
+package com.gustavoballeste.api_restful_jwt.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:message.properties")
+public class ApplicationConfiguration {
+}
